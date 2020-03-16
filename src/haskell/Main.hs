5a91417e15bc7836f047d267c364bff992d03334
Ian Denhardt <ian@zenhack.net>
2020-03-15 23:32:57 -0400

    Set content-type header for static assets.

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -4,8 +4,15 @@ import Zhp
 
 import Web.Scotty
 
+staticFiles =
+    [ ("/", "static/app.html", "text/html")
+    , ("/dndgrid.js", "build/dndgrid.js", "application/javascript")
+    , ("/setup.js", "static/setup.js", "application/javascript")
+    ]
+
 main :: IO ()
 main = scotty 3000 $ do
-    get "/" $ file "static/app.html"
-    get "/dndgrid.js" $ file "build/dndgrid.js"
-    get "/setup.js" $ file "static/setup.js"
+    for_ staticFiles $ \(url, filePath, contentType) ->
+        get url $ do
+            setHeader "Content-Type" contentType
+            file filePath
