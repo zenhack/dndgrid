0b5fd3c4b0de3ecf579bad77d71e15ba86a762f0
Ian Denhardt <ian@zenhack.net>
2020-05-26 18:32:29 -0400

    Add some event wrappers
    We'll want these when implementing the drawing feature.

--- src/elm/Events.elm
+++ src/elm/Events.elm
@@ -1,4 +1,12 @@
-module Events exposing (onChange, onDragEnter, onDragOver, onDragStart, onDrop)
+module Events exposing
+    ( Point
+    , onChange
+    , onDragEnter
+    , onDragOver
+    , onDragStart
+    , onDrop
+    , onMouseMove
+    )
 
 import Html exposing (Attribute)
 import Html.Events exposing (on, preventDefaultOn)
@@ -28,3 +36,29 @@ onDrop msg =
 onChange : (String -> msg) -> Attribute msg
 onChange mkMsg =
     on "change" (D.map mkMsg (D.field "target" (D.field "value" D.string)))
+
+
+type alias Point =
+    { x : Int
+    , y : Int
+    }
+
+
+decodePoint : D.Decoder Point
+decodePoint =
+    D.map2 Point
+        (D.field "offsetX" D.int)
+        (D.field "offsetY" D.int)
+
+
+onMouseMove : (Point -> msg) -> Attribute msg
+onMouseMove mkMsg =
+    on "mousemove" (D.map mkMsg decodePoint)
+
+
+{-| Handle the "mousedown" event. `elm/html` has a wrapper for this already,
+but it doesn't provide access to the location, so we have our own.
+-}
+onMouseDown : (Point -> msg) -> Attribute msg
+onMouseDown mkMsg =
+    on "mousedown" (D.map mkMsg decodePoint)
